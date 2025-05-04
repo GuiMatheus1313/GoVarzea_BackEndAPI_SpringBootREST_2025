e3d2ec771337a311c4fbc8baa2d9242c5534084a
GuiMatheus1313 <guilhermematheusdejesusaraujo6@gmail.com>
2025-05-04 14:15:12 -0300

    DOCS: CRUD Pessoa + correção Endpoints Urls

--- Modulo_Social/social/src/main/java/com/govarzeasocial/social/controller/PessoaController.java
+++ Modulo_Social/social/src/main/java/com/govarzeasocial/social/controller/PessoaController.java
@@ -2,6 +2,14 @@ package com.govarzeasocial.social.controller;
 
 import com.govarzeasocial.social.model.Pessoa;
 import com.govarzeasocial.social.service.PessoaService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jdk.jfr.ContentType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,36 +18,74 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/v1/pessoa")
+@Tag(name = "Pessoa", description = "Endpoints de Pessoa")
 public class PessoaController {
 
     @Autowired
     private PessoaService pessoaService;
 
-    @GetMapping("/all")
+    @Operation(summary = "Achar TODOS Pessoa", description = "Achar todas da tabela Pessoa")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "Sucesso em Achar todos", content = @Content(mediaType = "application/json", schema = @Schema(implementation = Pessoa.class))),
+                    @ApiResponse(responseCode = "500", description = "Erro no servidor interno", content = @Content)
+            }
+    )
+    @GetMapping("")
     public ResponseEntity<List<Pessoa>> findAll(){
         List<Pessoa> listPessoa = pessoaService.findall();
         return ResponseEntity.ok().body(listPessoa);
     }
 
+    @Operation(summary = "Achar UMA Pessoa", description = "Achar UMA da tabela Pessoa")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "Sucesso em Achar", content = @Content(mediaType = "application/json", schema = @Schema(implementation = Pessoa.class))),
+                    @ApiResponse(responseCode = "404", description = "Pessoa não Encontrada", content = @Content),
+                    @ApiResponse(responseCode = "500", description = "Erro no servidor interno", content = @Content)
+            }
+    )
     @GetMapping(value ="/{id}")
-    public ResponseEntity<Pessoa> findById(@PathVariable String id){
+    public ResponseEntity<Pessoa> findById(@Parameter (description = "Cpf da Pessoa", required = true) @PathVariable String id){
         return ResponseEntity.ok().body(pessoaService.findById(id));
     }
 
-    @PostMapping("/create")
-    public ResponseEntity<Pessoa> insert(@RequestBody Pessoa pessoa){
+
+    @Operation(summary = "Cadastrar UMA Pessoa", description = "Cadastrar UMA da tabela Pessoa")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "Sucesso em Cadastrar", content = @Content(mediaType = "application/json", schema = @Schema(implementation = Pessoa.class))),
+                    @ApiResponse(responseCode = "500", description = "Erro no servidor interno", content = @Content)
+            }
+    )
+    @PostMapping()
+    public ResponseEntity<Pessoa> insert(@io.swagger.v3.oas.annotations.parameters.RequestBody(description = "Dados Pessoa", required = true, content = @Content(schema = @Schema(implementation = Pessoa.class)))@RequestBody Pessoa pessoa){
         pessoa = pessoaService.insert(pessoa);
         return ResponseEntity.ok().body(pessoa);
     }
 
+    @Operation(summary = "Editar UMA Pessoa", description = "Editar UMA da tabela Pessoa")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "Sucesso em Editar", content = @Content(mediaType = "application/json", schema = @Schema(implementation = Pessoa.class))),
+                    @ApiResponse(responseCode = "500", description = "Erro no servidor interno", content = @Content)
+            }
+    )
     @PutMapping("/{id}")
-    public ResponseEntity<Pessoa> update(@PathVariable String id, @RequestBody Pessoa pessoa){
+    public ResponseEntity<Pessoa> update(@Parameter (description = "Cpf da Pessoa", required = true)@PathVariable String id, @io.swagger.v3.oas.annotations.parameters.RequestBody(description = "Dados Pessoa", required = true, content = @Content(schema = @Schema(implementation = Pessoa.class))) @RequestBody Pessoa pessoa){
         pessoa = pessoaService.edit(id, pessoa);
         return ResponseEntity.ok().body(pessoa);
     }
 
+    @Operation(summary = "Deletar UMA Pessoa", description = "Deletar UMA da tabela Pessoa")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "Sucesso em Deletar", content = @Content(mediaType = "application/json", schema = @Schema(implementation = Pessoa.class))),
+                    @ApiResponse(responseCode = "500", description = "Erro no servidor interno", content = @Content)
+            }
+    )
     @DeleteMapping("/{id}")
-    public ResponseEntity<String> delete(@PathVariable String id){
+    public ResponseEntity<String> delete(@Parameter (description = "Cpf da Pessoa", required = true) @PathVariable String id){
         return ResponseEntity.ok().body(pessoaService.delete(id));
 
     }
