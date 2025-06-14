87be8c09110e1a1cad5d1c29fe86533e7fccf64b
GuiMatheus1313 <guilhermematheusdejesusaraujo6@gmail.com>
2025-06-14 13:14:59 -0300

    FIX: CustomUserDetails não sendo configurado

--- Modulo_Social/social/src/main/java/com/govarzeasocial/social/service/security/CustomUserDetailsService.java
+++ Modulo_Social/social/src/main/java/com/govarzeasocial/social/service/security/CustomUserDetailsService.java
@@ -9,9 +9,12 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+//SERVIÇO PARA CARREGAR O USER OU COMO VAMOS FAZER ELE
+@Service
 public class CustomUserDetailsService implements UserDetailsService {
     @Autowired
     private PessoaRepo pessoaRepo;
